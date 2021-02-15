87c621c568ea491b0c07d76d011f22b5107cfabf
Benito Palacios Sanchez <benito356@gmail.com>
2021-02-15 00:53:17 +0000

    :sparkles: Support LZSSD decompression

---
+++ src/LayTea.Tests/Containers/LzssdDecompressionTests.cs
@@ -0,0 +1,159 @@
+// Copyright (c) 2021 SceneGate
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+namespace SceneGate.Games.ProfessorLayton.Tests.Containers
+{
+    using System.Collections;
+    using NUnit.Framework;
+    using SceneGate.Games.ProfessorLayton.Containers;
+    using Yarhl.IO;
+
+    [TestFixture]
+    public class LzssdDecompressionTests
+    {
+        private LzssdDecompression decompression;
+
+        [OneTimeSetUp]
+        public void SetUpFixture()
+        {
+            decompression = new LzssdDecompression();
+        }
+
+        [Test]
+        public void NullDecompressionThrowsException()
+        {
+            Assert.That(() => decompression.Convert((BinaryFormat)null), Throws.ArgumentNullException);
+            Assert.That(() => decompression.Convert((DataStream)null), Throws.ArgumentNullException);
+        }
+
+        [TestCaseSource(nameof(GetCompressedFiles))]
+        public void DecompressionWithFiles(string inputPath, long offset, long length, string infoPath)
+        {
+            TestDataBase.IgnoreIfFileDoesNotExist(inputPath);
+            TestDataBase.IgnoreIfFileDoesNotExist(infoPath);
+
+            using var input = new BinaryFormat(DataStreamFactory.FromFile(inputPath, FileOpenMode.Read, offset, length));
+            var expected = BinaryInfo.FromYaml(infoPath);
+
+            BinaryFormat decompressed = null;
+            try {
+                Assert.That(() => decompressed = decompression.Convert(input), Throws.Nothing);
+                expected.AssertIsEqual(decompressed);
+            } finally {
+                decompressed?.Dispose();
+            }
+        }
+
+        [Test]
+        public void CopyRawToken()
+        {
+            var stream = new DataStream();
+            stream.Write(new byte[] { 0x06, 0xCA, 0xFE, 0xBE }, 0, 4);
+
+            using var decompressed = decompression.Convert(stream);
+            decompressed.Position = 0;
+
+            Assert.That(decompressed.Length, Is.EqualTo(3));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xCA));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xFE));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xBE));
+        }
+
+        [Test]
+        public void CopyRawTokenZeroLength()
+        {
+            var stream = new DataStream();
+            stream.Write(new byte[] { 0x00 }, 0, 1);
+
+            using var decompressed = decompression.Convert(stream);
+
+            Assert.That(decompressed.Length, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CopyBufferTokenFullPast()
+        {
+            var stream = new DataStream();
+            stream.Write(new byte[] { 0x06, 0xCA, 0xFE, 0xBE, 0x07, 0x10 }, 0, 6);
+
+            using var decompressed = decompression.Convert(stream);
+            decompressed.Position = 0;
+
+            Assert.That(decompressed.Length, Is.EqualTo(6));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xCA));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xFE));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xBE));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xCA));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xFE));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xBE));
+        }
+
+        [Test]
+        public void CopyBufferTokenWithFuture()
+        {
+            var stream = new DataStream();
+            stream.Write(new byte[] { 0x06, 0xCA, 0xFE, 0xBE, 0x05, 0x20 }, 0, 6);
+
+            using var decompressed = decompression.Convert(stream);
+            decompressed.Position = 0;
+
+            Assert.That(decompressed.Length, Is.EqualTo(7));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xCA));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xFE));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xBE));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xFE));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xBE));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xFE));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xBE));
+        }
+
+        [Test]
+        public void CopyBufferTokenFullFuture()
+        {
+            var stream = new DataStream();
+            stream.Write(new byte[] { 0x06, 0xCA, 0xFE, 0xBE, 0x03, 0x20 }, 0, 6);
+
+            using var decompressed = decompression.Convert(stream);
+            decompressed.Position = 0;
+
+            Assert.That(decompressed.Length, Is.EqualTo(7));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xCA));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xFE));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xBE));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xBE));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xBE));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xBE));
+            Assert.That(decompressed.ReadByte(), Is.EqualTo(0xBE));
+        }
+
+        [Test]
+        public void CopyBufferTokenPosZeroThrows()
+        {
+            var stream = new DataStream();
+            stream.Write(new byte[] { 0x06, 0xCA, 0xFE, 0xBE, 0x01, 0x20 }, 0, 6);
+
+            Assert.That(() => decompression.Convert(stream), Throws.InvalidOperationException);
+        }
+
+        private static IEnumerable GetCompressedFiles()
+        {
+            return TestData.GetSubstreamAndInfoCollection("lzssd.txt");
+        }
+    }
+}
---
+++ src/LayTea/Containers/LzssdDecompression.cs
@@ -0,0 +1,107 @@
+// Copyright (c) 2021 SceneGate
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+namespace SceneGate.Games.ProfessorLayton.Containers
+{
+    using System;
+    using Yarhl.FileFormat;
+    using Yarhl.IO;
+
+    /// <summary>
+    /// Decompression algorithm for LZSS variant found in DENC formats.
+    /// </summary>
+    public class LzssdDecompression : IConverter<BinaryFormat, BinaryFormat>
+    {
+        /// <summary>
+        /// Decompress a LZSS-DENC compressed stream.
+        /// </summary>
+        /// <param name="source">The compressed stream with LZSS-DENC.</param>
+        /// <returns>The decompressed stream.</returns>
+        public BinaryFormat Convert(BinaryFormat source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            var decompressed = Convert(source.Stream);
+            return new BinaryFormat(decompressed);
+        }
+
+        /// <summary>
+        /// Decompress a LZSS-DENC compressed stream.
+        /// </summary>
+        /// <param name="source">The compressed stream with LZSS-DENC.</param>
+        /// <returns>The decompressed stream.</returns>
+        public DataStream Convert(DataStream source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            DataStream output = new DataStream();
+
+            source.Position = 0;
+            var reader = new DataReader(source);
+            while (!source.EndOfStream) {
+                byte token = reader.ReadByte();
+                if ((token & 0x01) == 0) {
+                    // Copy from input a sequence of bytes.
+                    int length = token >> 1;
+                    CopyRaw(reader, length, output);
+                } else {
+                    // Repeat a sequence of bytes from output.
+                    int flags = token | (reader.ReadByte() << 8);
+                    int pos = (flags >> 1) & 0x7FF;
+                    int length = (flags >> 12) + 2;
+                    CopyOutput(pos, length, output);
+                }
+            }
+
+            return output;
+        }
+
+        private void CopyRaw(DataReader reader, int length, DataStream output)
+        {
+            output.Write(reader.ReadBytes(length));
+        }
+
+        private void CopyOutput(int startBackPos, int length, DataStream output)
+        {
+            if (startBackPos == 0) {
+                throw new InvalidOperationException("Invalid copy position 0");
+            }
+
+            long startCopyPos = output.Position - startBackPos;
+            long startOutPos = output.Position;
+            int pastDataLength = (int)(startOutPos - startCopyPos);
+            byte[] buffer = new byte[1024];
+
+            int written = 0;
+            while (written < length) {
+                int toCopy = (length - written < pastDataLength) ? length - written : pastDataLength;
+
+                output.Position = startCopyPos + written;
+                output.Read(buffer, 0, toCopy);
+
+                output.Position = startOutPos + written;
+                output.Write(buffer, 0, toCopy);
+
+                written += toCopy;
+            }
+        }
+    }
+}
