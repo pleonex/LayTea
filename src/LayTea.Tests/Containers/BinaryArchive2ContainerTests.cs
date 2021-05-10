031ab2244dbe100ba9f453c6a1da6bbd625e882d
Benito Palacios Sanchez <benito356@gmail.com>
2021-05-10 21:58:04 +0200

    :sparkles: Implement ARCHIVE container

---
+++ src/LayTea.Tests/Containers/BinaryArchive2ContainerTests.cs
@@ -0,0 +1,76 @@
+// Copyright (c) 2021 SceneGate
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+namespace SceneGate.Games.ProfessorLayton.Tests.Containers
+{
+    using System;
+    using System.Collections;
+    using System.IO;
+    using NUnit.Framework;
+    using SceneGate.Games.ProfessorLayton.Containers;
+    using Yarhl.FileFormat;
+    using Yarhl.FileSystem;
+    using Yarhl.IO;
+
+    [TestFixtureSource(typeof(BinaryArchive2ContainerTests), nameof(BinaryArchive2ContainerTests.TestsInfo))]
+    public class BinaryArchive2ContainerTests : Binary2ContainerTests
+    {
+        private readonly string yamlPath;
+        private readonly string binaryPath;
+
+        public BinaryArchive2ContainerTests(string yamlPath, string binaryPath)
+        {
+            this.yamlPath = yamlPath;
+            this.binaryPath = binaryPath;
+
+            TestDataBase.IgnoreIfFileDoesNotExist(yamlPath);
+            TestDataBase.IgnoreIfFileDoesNotExist(binaryPath);
+        }
+
+        public static IEnumerable TestsInfo {
+            get => TestData.GetStreamAndInfoCollection("archive.txt");
+        }
+
+        [Test]
+        public void NotBinaryChildThrowsException()
+        {
+            using var container = new NodeContainerFormat();
+            container.Root.Add(new Node("child", new NodeContainerFormat()));
+
+            var converter = GetToBinaryConverter();
+            Assert.That(() => converter.Convert(container), Throws.InstanceOf<FormatException>());
+        }
+
+        protected override BinaryFormat GetBinary()
+        {
+            TestContext.WriteLine(Path.GetFileName(binaryPath));
+            var stream = DataStreamFactory.FromFile(binaryPath, FileOpenMode.Read);
+            return new BinaryFormat(stream);
+        }
+
+        protected override NodeContainerInfo GetContainerInfo() =>
+            NodeContainerInfo.FromYaml(yamlPath);
+
+        protected override IConverter<NodeContainerFormat, BinaryFormat> GetToBinaryConverter() =>
+            new Container2BinaryArchive();
+
+        protected override IConverter<BinaryFormat, NodeContainerFormat> GetToContainerConverter() =>
+            new BinaryArchive2Container();
+    }
+}
