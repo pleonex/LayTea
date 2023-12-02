dc69a596168ab36961316f432017b6bdae5e6283
Benito Palacios Sanchez <pleonex@protonmail.com>
2023-12-02 11:39:55 +0100

    :umbrella: Helper methods to create tests

--- src/LayTea.Tests/Assertions/NodeAssertions.cs
+++ src/LayTea.Tests/Assertions/NodeAssertions.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 SceneGate
+﻿// Copyright (c) 2021 SceneGate
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -17,55 +17,55 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+namespace SceneGate.Games.ProfessorLayton.Tests.Assertions;
+
 using FluentAssertions;
 using FluentAssertions.Execution;
 using FluentAssertions.Primitives;
+using SceneGate.Games.ProfessorLayton.Tests;
 using Yarhl.FileSystem;
 
-namespace SceneGate.Games.ProfessorLayton.Tests.Assertions
+public class NodeAssertions
+    : ReferenceTypeAssertions<Node, NodeAssertions>
 {
-    public class NodeAssertions
-        : ReferenceTypeAssertions<Node, NodeAssertions>
+    public NodeAssertions(Node instance)
+        : base(instance)
     {
-        public NodeAssertions(Node instance)
-            : base(instance)
-        {
+    }
+
+    protected override string Identifier => "node";
+
+    [CustomAssertion]
+    public AndConstraint<NodeAssertions> MatchInfo(NodeContainerInfo info)
+    {
+        Subject.Name.Should().Be(info.Name);
+        Subject.Format?.GetType().FullName.Should().Be(info.FormatType);
+
+        if (info.Tags != null) {
+            // YAML deserializer always gets the value as a string
+            foreach (var entry in info.Tags) {
+                Subject.Tags.Should().ContainKey(entry.Key);
+                Subject.Tags[entry.Key].ToString().Should().Be(entry.Value);
+            }
         }
 
-        protected override string Identifier => "node";
-
-        [CustomAssertion]
-        public AndConstraint<NodeAssertions> MatchInfo(NodeContainerInfo info)
-        {
-            Subject.Name.Should().Be(info.Name);
-            Subject.Format?.GetType().FullName.Should().Be(info.FormatType);
-
-            if (info.Tags != null) {
-                // YAML deserializer always gets the value as a string
-                foreach (var entry in info.Tags) {
-                    Subject.Tags.Should().ContainKey(entry.Key);
-                    Subject.Tags[entry.Key].ToString().Should().Be(entry.Value);
-                }
-            }
-
-            if (info.Stream != null) {
-                Subject.Stream.Should().MatchInfo(info.Stream);
-            }
-
-            int expectedCount = info.Children?.Count ?? 0;
-            if (info.CheckChildren) {
-                Subject.Children.Should().HaveCount(expectedCount);
-            }
-
-            for (int i = 0; i < expectedCount; i++) {
-                NodeContainerInfo expectedNode = info.Children[i];
-                using (new AssertionScope(expectedNode.Name)) {
-                    Subject.Children.Should().Contain(n => n.Name == expectedNode.Name);
-                    Subject.Children[expectedNode.Name].Should().MatchInfo(expectedNode);
-                }
-            }
-
-            return new AndConstraint<NodeAssertions>(this);
+        if (info.Stream != null) {
+            Subject.Stream.Should().MatchInfo(info.Stream);
         }
+
+        int expectedCount = info.Children?.Count ?? 0;
+        if (info.CheckChildren) {
+            Subject.Children.Should().HaveCount(expectedCount);
+        }
+
+        for (int i = 0; i < expectedCount; i++) {
+            NodeContainerInfo expectedNode = info.Children[i];
+            using (new AssertionScope(expectedNode.Name)) {
+                Subject.Children.Should().Contain(n => n.Name == expectedNode.Name);
+                Subject.Children[expectedNode.Name].Should().MatchInfo(expectedNode);
+            }
+        }
+
+        return new AndConstraint<NodeAssertions>(this);
     }
 }
